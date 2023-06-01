092d6e65b7ef581abf8ab329ffb0fadd9d7b5d59
Jean0405 <jeanpierrea0405@gmail.com>
2023-06-01 11:47:24 -0500

    feat: :sparkles: se desarrollaron los módulos para realizar los calculos
    Se crearon las funciones y validaciones necesarias para llevar a cabo operaciones de forma correcta

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
 <?php
     session_start();
+
+    function sumar($n1, $n2){
+        $_SESSION['num1'] = ($n1 + $n2);
+    }
+
+    function restar($n1, $n2){
+        $_SESSION['num1'] = ($n1 - $n2);
+    }
+
+    function multiplicar($n1, $n2){
+        $_SESSION['num1'] = ($n1 * $n2);
+    }
+
+    function dividir($n1, $n2){
+        if ($n2 == 0) {
+            $_SESSION['num1'] = "Error";
+        }else {            
+             $_SESSION['num1'] = ($n1 / $n2);
+        }
+    }
+
+
     if (isset($_POST['numero'])) {
         if($_POST['numero'] == "c"){
             $_SESSION['num1'] = null;
+        }else if($_POST['numero'] == "+" || $_POST['numero'] == "-" || $_POST['numero'] == "*" || $_POST['numero'] == "/"){
+            $_SESSION['valor1'] =  $_SESSION['num1'];
+            $_SESSION['operacion'] = $_POST['numero'];
+            $_SESSION['num1'] = null;
         }else if($_POST['numero'] == "←"){
             $_SESSION['num1'] = substr($_SESSION['num1'],0, -1);
+        }else if($_POST['numero'] == "="){
+            $_SESSION['valor2'] = $_SESSION['num1'];
+            switch ($_SESSION['operacion']) {
+                case '+':
+                    sumar($_SESSION['valor1'], $_SESSION['valor2']);
+                    break;
+                case '-':
+                    restar($_SESSION['valor1'], $_SESSION['valor2']);
+                    break;
+                case '*':
+                    multiplicar($_SESSION['valor1'], $_SESSION['valor2']);
+                    break;
+                case '/':
+                    dividir($_SESSION['valor1'], $_SESSION['valor2']);
+                    break;
+            }
         }else{
             if (isset($_SESSION['num1'])) {
                 $_SESSION['num1'] .= $_POST['numero'];
@@ -29,8 +71,9 @@
     <form class="container-fluid bg-warning" method="POST">
         <div class="row justify-content-center" style="height:100vh">
             <div class="col-12 d-flex flex-column justify-content-center align-items-center">
-            <input class="col-10 col-sm-6 col-md-3 col-lg-3 my-2 p-2 rounded-pill border-0" style="outline:none;" type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>">
-                <div class="col-12 col-sm-8 col-md-5 col-lg-5 d-flex justify-content-center">
+                <h1 class="fw-bold text-light">CALCULADORA</h1>
+                <input class="col-10 col-sm-6 col-md-3 col-lg-3 my-2 p-2 rounded-pill border-0" style="outline:none;" type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>">
+                <div class="col-12 col-sm-8 col-md-5 col-lg-5 mt-4 d-flex justify-content-center">
                     <input class="btn btn-dark col-1 m-1 p-1" type="submit" name="numero" value="7">
                     <input class="btn btn-dark col-1 m-1 p-1" type="submit" name="numero" value="8">
                     <input class="btn btn-dark col-1 m-1 p-1" type="submit" name="numero" value="9">
@@ -49,24 +92,17 @@
                     <input class="btn btn-danger col-1 m-1 p-1" type="submit" name="numero" value="-">
                 </div>
                 <div class="col-12 col-sm-8 col-md-5 col-lg-5 d-flex justify-content-center m-1">
-                    <input class="btn btn-dark col-1 m-1 p-1" type="submit" name="numero" value="0">
                     <input class="btn btn-danger col-1 m-1 p-1" type="submit" name="numero" value="←">
-                    <input class="btn btn-danger col-1 m-1 p-1" type="submit" name="numero" value="C">
-                    <input class="btn btn-success col-1 m-1 p-1" type="submit" name="numero" value="=">
+                    <input class="btn btn-dark col-1 m-1 p-1" type="submit" name="numero" value="0">
+                    <input class="btn btn-danger fw-bold col-1 m-1 p-1" type="submit" name="numero" value=",">
+                    <input class="btn btn-danger col-1 m-1 p-1" type="submit" name="numero" value="+">
+                </div>
+                <div class="col-12 col-sm-8 col-md-5 col-lg-5 d-flex justify-content-center m-1">
+                    <input class="btn btn-secondary col-1 m-1 p-1" type="submit" name="numero" value="c">
+                    <input class="btn btn-success col-3 m-1 ms-3 p-1" type="submit" name="numero" value="=">
                 </div>
             </div>
         </div>
-        
-        <!-- <div class="row">
-            <div class="col-12">
-                <button type="submit" name="numero" value="1">1</button>
-                <button type="submit" name="numero" value="2">2</button>
-                <button type="submit" name="numero" value="3">3</button>
-                <button type="submit" name="numero" value="c">c</button>
-                <button type="submit" name="numero" value="←">←</button>
-                <input type="submit" value="Enviar">
-            </div>
-        </div> -->
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
